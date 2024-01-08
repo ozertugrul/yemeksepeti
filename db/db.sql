5ad9c960769dbf1b3a9d16953bc704a56a8b1d2b
AbdullahDoğan <abdogan0306mail.com>
2024-01-09 00:39:52 +0300

    veritabanını güncelledim

--- db/db.sql
+++ db/db.sql
@@ -1,38 +1,269 @@
--- Kullanıcılar tablosu
-CREATE TABLE Kullanicilar (
-    KullaniciID INT PRIMARY KEY AUTO_INCREMENT,
-    Adi VARCHAR(50) NOT NULL,
-    Soyadi VARCHAR(50) NOT NULL,
-    Eposta VARCHAR(100) NOT NULL UNIQUE,
-    Sifre VARCHAR(100) NOT NULL,
-    TelNo VARCHAR(100) NOT NULL,
-    dogumTarihi VARCHAR(100) NOT NULL
-);
+-- phpMyAdmin SQL Dump
+-- version 4.8.3
+-- https://www.phpmyadmin.net/
+--
+-- Anamakine: 127.0.0.1
+-- Üretim Zamanı: 08 Oca 2024, 20:10:56
+-- Sunucu sürümü: 10.1.36-MariaDB
+-- PHP Sürümü: 5.6.38
 
-CREATE TABLE Dukkanlar (
-    dukkan_id INT PRIMARY KEY AUTO_INCREMENT,
-    dukkan_adi VARCHAR(100) NOT NULL,
-    dukkan_adresi VARCHAR(255) NOT NULL
-    dukkkan_sehiri VARCHAR(100) NOT NULL,
-    dukkan_mstutar INT,
-);
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+SET AUTOCOMMIT = 0;
+START TRANSACTION;
+SET time_zone = "+00:00";
 
--- Ürünler tablosu
-CREATE TABLE Urunler (
-    UrunID INT PRIMARY KEY AUTO_INCREMENT,
-    UrunAdi VARCHAR(100) NOT NULL,
-    Fiyat DECIMAL(10, 2) NOT NULL,
-    MagazaID INT,
-    FOREIGN KEY (MagazaID) REFERENCES Magazalar(MagazaID)
-);
 
--- Siparişler tablosu
-CREATE TABLE Siparisler (
-    SiparisID INT PRIMARY KEY AUTO_INCREMENT,
-    KullaniciID INT,
-    UrunID INT,
-    Adet INT,
-    Tarih TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
-    FOREIGN KEY (KullaniciID) REFERENCES Kullanicilar(KullaniciID),
-    FOREIGN KEY (UrunID) REFERENCES Urunler(UrunID)
-);
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Veritabanı: `yemek`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Tablo için tablo yapısı `dukkanlar`
+--
+
+CREATE TABLE `dukkanlar` (
+  `dukkan_id` int(11) NOT NULL,
+  `dukkan_resmi_adi` varchar(255) COLLATE utf8_turkish_ci NOT NULL,
+  `dukkan_adi` varchar(100) COLLATE utf8_turkish_ci NOT NULL,
+  `dukkan_adresi` varchar(255) COLLATE utf8_turkish_ci NOT NULL,
+  `dukkkan_sehiri` varchar(100) COLLATE utf8_turkish_ci NOT NULL,
+  `dukkan_mstutar` int(11) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_turkish_ci;
+
+
+
+-- --------------------------------------------------------
+
+--
+-- Tablo için tablo yapısı `kullanicilar`
+--
+
+CREATE TABLE `kullanicilar` (
+  `KullaniciID` int(11) NOT NULL,
+  `Adi` varchar(50) COLLATE utf8_turkish_ci NOT NULL,
+  `Soyadi` varchar(50) COLLATE utf8_turkish_ci NOT NULL,
+  `Eposta` varchar(100) COLLATE utf8_turkish_ci NOT NULL,
+  `Sifre` varchar(100) COLLATE utf8_turkish_ci NOT NULL,
+  `cep_telefonu` varchar(100) COLLATE utf8_turkish_ci NOT NULL,
+  `dogum_tarihi` varchar(100) COLLATE utf8_turkish_ci NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_turkish_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Tablo için tablo yapısı `sehirler`
+--
+
+CREATE TABLE `sehirler` (
+  `Id` int(11) NOT NULL,
+  `SehirAd` varchar(50) CHARACTER SET utf8 NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Tablo döküm verisi `sehirler`
+--
+
+INSERT INTO `sehirler` (`Id`, `SehirAd`) VALUES
+(1, 'Adana'),
+(2, 'Adıyaman'),
+(3, 'Afyonkarahisar'),
+(4, 'Ağrı'),
+(5, 'Amasya'),
+(6, 'Ankara'),
+(7, 'Antalya'),
+(8, 'Artvin'),
+(9, 'Aydın'),
+(10, 'Balıkesir'),
+(11, 'Bilecik'),
+(12, 'Bingöl'),
+(13, 'Bitlis'),
+(14, 'Bolu'),
+(15, 'Burdur'),
+(16, 'Bursa'),
+(17, 'Çanakkale'),
+(18, 'Çankırı'),
+(19, 'Çorum'),
+(20, 'Denizli'),
+(21, 'Diyarbakır'),
+(22, 'Edirne'),
+(23, 'Elazığ'),
+(24, 'Erzincan'),
+(25, 'Erzurum'),
+(26, 'Eskişehir'),
+(27, 'Gaziantep'),
+(28, 'Giresun'),
+(29, 'Gümüşhane'),
+(30, 'Hakkari'),
+(31, 'Hatay'),
+(32, 'Isparta'),
+(33, 'Mersin'),
+(34, 'İstanbul'),
+(35, 'İzmir'),
+(36, 'Kars'),
+(37, 'Kastamonu'),
+(38, 'Kayseri'),
+(39, 'Kırklareli'),
+(40, 'Kırşehir'),
+(41, 'Kocaeli'),
+(42, 'Konya'),
+(43, 'Kütahya'),
+(44, 'Malatya'),
+(45, 'Manisa'),
+(46, 'Kahramanmaraş'),
+(47, 'Mardin'),
+(48, 'Muğla'),
+(49, 'Muş'),
+(50, 'Nevşehir'),
+(51, 'Niğde'),
+(52, 'Ordu'),
+(53, 'Rize'),
+(54, 'Sakarya'),
+(55, 'Samsun'),
+(56, 'Siirt'),
+(57, 'Sinop'),
+(58, 'Sivas'),
+(59, 'Tekirdağ'),
+(60, 'Tokat'),
+(61, 'Trabzon'),
+(62, 'Tunceli'),
+(63, 'Şanlıurfa'),
+(64, 'Uşak'),
+(65, 'Van'),
+(66, 'Yozgat'),
+(67, 'Zonguldak'),
+(68, 'Aksaray'),
+(69, 'Bayburt'),
+(70, 'Karaman'),
+(71, 'Kırıkkale'),
+(72, 'Batman'),
+(73, 'Şırnak'),
+(74, 'Bartın'),
+(75, 'Ardahan'),
+(76, 'Iğdır'),
+(77, 'Yalova'),
+(78, 'Karabük'),
+(79, 'Kilis'),
+(80, 'Osmaniye'),
+(81, 'Düzce');
+
+-- --------------------------------------------------------
+
+--
+-- Tablo için tablo yapısı `siparisler`
+--
+
+CREATE TABLE `siparisler` (
+  `SiparisID` int(11) NOT NULL,
+  `KullaniciID` int(11) DEFAULT NULL,
+  `UrunID` int(11) DEFAULT NULL,
+  `Adet` int(11) DEFAULT NULL,
+  `Tarih` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_turkish_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Tablo için tablo yapısı `urunler`
+--
+
+CREATE TABLE `urunler` (
+  `UrunID` int(11) NOT NULL,
+  `UrunAdi` varchar(100) COLLATE utf8_turkish_ci NOT NULL,
+  `Fiyat` decimal(10,2) NOT NULL,
+  `DukkanID` int(11) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_turkish_ci;
+
+--
+-- Dökümü yapılmış tablolar için indeksler
+--
+
+--
+-- Tablo için indeksler `dukkanlar`
+--
+ALTER TABLE `dukkanlar`
+  ADD PRIMARY KEY (`dukkan_id`);
+
+--
+-- Tablo için indeksler `kullanicilar`
+--
+ALTER TABLE `kullanicilar`
+  ADD PRIMARY KEY (`KullaniciID`),
+  ADD UNIQUE KEY `Eposta` (`Eposta`);
+
+--
+-- Tablo için indeksler `sehirler`
+--
+ALTER TABLE `sehirler`
+  ADD PRIMARY KEY (`Id`);
+
+--
+-- Tablo için indeksler `siparisler`
+--
+ALTER TABLE `siparisler`
+  ADD PRIMARY KEY (`SiparisID`),
+  ADD KEY `KullaniciID` (`KullaniciID`),
+  ADD KEY `UrunID` (`UrunID`);
+
+--
+-- Tablo için indeksler `urunler`
+--
+ALTER TABLE `urunler`
+  ADD PRIMARY KEY (`UrunID`),
+  ADD KEY `DukkanID` (`DukkanID`);
+
+--
+-- Dökümü yapılmış tablolar için AUTO_INCREMENT değeri
+--
+
+--
+-- Tablo için AUTO_INCREMENT değeri `dukkanlar`
+--
+ALTER TABLE `dukkanlar`
+  MODIFY `dukkan_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+
+--
+-- Tablo için AUTO_INCREMENT değeri `kullanicilar`
+--
+ALTER TABLE `kullanicilar`
+  MODIFY `KullaniciID` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- Tablo için AUTO_INCREMENT değeri `siparisler`
+--
+ALTER TABLE `siparisler`
+  MODIFY `SiparisID` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- Tablo için AUTO_INCREMENT değeri `urunler`
+--
+ALTER TABLE `urunler`
+  MODIFY `UrunID` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- Dökümü yapılmış tablolar için kısıtlamalar
+--
+
+--
+-- Tablo kısıtlamaları `siparisler`
+--
+ALTER TABLE `siparisler`
+  ADD CONSTRAINT `siparisler_ibfk_1` FOREIGN KEY (`KullaniciID`) REFERENCES `kullanicilar` (`KullaniciID`),
+  ADD CONSTRAINT `siparisler_ibfk_2` FOREIGN KEY (`UrunID`) REFERENCES `urunler` (`UrunID`);
+
+--
+-- Tablo kısıtlamaları `urunler`
+--
+ALTER TABLE `urunler`
+  ADD CONSTRAINT `urunler_ibfk_1` FOREIGN KEY (`DukkanID`) REFERENCES `dukkanlar` (`dukkan_id`);
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
